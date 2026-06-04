801cbf72cb36cf2b7a9fe417dd4ad973e82620e1
AndrewBarquero <a.barquero.6@estudiantec.cr>
2026-06-03 21:29:54 -0600

    Falta perfeccionar

--- Codigo_fuente_P2_FSC.py
+++ Codigo_fuente_P2_FSC.py
@@ -20,6 +20,98 @@ import socket  #Libreria para conectar la computadora a la rasberry por medio de
 import threading #Libreria para ejecutar 2 instrucciones a la vez
 import time  #Libreria que se usa para hacer tiempos entre funciones 
 
+#El codigo que se usa para hacer la conexion camputadora-rasberry fue proporcionado por el profesor Luis Barboza. Todos los creditos a él.
+#Solo se hicieron modificaciones a su codigo
+#=====================Aqui se pondra el codigo para conectar la interfaz con la rasberry===================================
+
+# CONFIGURACIoN DE RED
+#la ip cambia, es importante poner la ip que te da la rasberry
+SERVER_IP = '10.215.141.165'
+# puerto de comunicacion
+# ambos programas deben usar el MISMO puerto
+PORT = 1717
+
+# CREAR SOCKET CLIENTE:
+# AF_INET:
+# usar IPv4
+#
+# SOCK_STREAM:
+# usar TCP
+#
+# TCP:
+# conexion estable y segura
+# parecida a una llamada telefonica
+# ============================================================
+
+client_socket = socket.socket(
+    socket.AF_INET,
+    socket.SOCK_STREAM
+)
+#Funcion que se conecta al servidor
+def connect():
+    try:
+        #Intenta conectarse a la rasberry
+        client_socket.connect((SERVER_IP, PORT))
+        # CREAR HILO PARA RECIBIR MENSAJES:
+        # daemon=True:
+        # el hilo se cerrara automaticamente
+        # cuando cierre el programa
+        threading.Thread(
+            target=receive_messages,
+            daemon=True
+        ).start()
+
+        print("Conectado al servidor")
+
+    except Exception as e:
+        # mostrar error si falla conexion
+        print(f"Error: {e}")
+
+#Funcion que envia un mensaje a la rasberry
+def send_message():
+    print("Hola")
+    # obtener texto escrito en Entry
+    msg = "Este mensaje es para la rasberry"
+
+    # verificar que no este vacio
+    if msg != "":
+        # ENVIAR MENSAJE:
+        # encode():
+        # convierte texto a bytes
+        # internet trabaja con bytes
+        client_socket.send(msg.encode()) #esta linea es la que envia el mensaje a la rasberry
+        print("se ha enviado el mensaje")
+
+#Funcion que recibe mensajes de la rasberry
+def receive_messages():
+    while True:
+        #Prueba si hay hay algun mensaje que recibir, si no, cierra el loop
+        try:
+            # RECIBIR DATOS
+            # 1024:maximo de bytes a recibir
+            msg = client_socket.recv(1024).decode()
+            #Imprime el mensaje que recibe
+            print(f"Raspberry: {msg}\n")
+        except:
+            break
+
+#Funcion que termina la conexion en caso de que se cierre el programa
+def salir():
+    #Prueba si se puede cerrar, si no se puede es porque yano esta abierto y pasa
+    try:
+        # cerrar socket
+        client_socket.close()
+    except:
+        pass
+    ventana_principal.destroy()
+
+
+#Iniciar la conexion
+connect()
+
+#==========       ============          ==============         ===========     ==============      =======================
+
+
 ventana_principal = tk.Tk() #Se crea la ventana
 ventana_principal.geometry("625x640+750+250") #Define las dimensiones de la ventana
 ventana_principal.title("Maquina Expendedora") #Le asigna un titulo a la ventana
@@ -30,6 +122,10 @@ imagen_tk = ImageTk.PhotoImage(imagen_inicio)   #Trae la imagen abierta
 imagen_Pantalla_inicio = tk.Label(ventana_principal, image=imagen_tk) #Le asigna un label a la imagen
 imagen_Pantalla_inicio.pack() #Coloca el label en la entana
 
+tk.Label(imagen_Pantalla_inicio, text="PROYECTO #2 \n FSC\nAndrew Barquero\n Guillermo Mora", 
+         bg="#445a4e", fg="#cef3c4", justify="center",
+        font=("Arial", 13, "bold")).place(relx=0.67, rely=0.1)
+
 
 boton_mantenimiento = tk.Button(imagen_Pantalla_inicio, width=12, 
                         relief="groove",bd=5, bg="#383447", 
@@ -38,11 +134,11 @@ boton_mantenimiento = tk.Button(imagen_Pantalla_inicio, width=12,
 boton_estadistica = tk.Button(imagen_Pantalla_inicio, width=6, 
                         relief="groove",bd=5, bg="#383447", 
                         fg="White", text="Datos", 
-                        command=ventana_principal.destroy).place(relx= 0.67, rely=0.625)
+                        command=send_message).place(relx= 0.67, rely=0.625)
 boton_salir = tk.Button(imagen_Pantalla_inicio, width=6, 
                         relief="groove",bd=5, bg="#383447", 
                         fg="white", text="Salir", 
-                        command=ventana_principal.destroy).place(relx= 0.78, rely=0.625)
+                        command=salir).place(relx= 0.78, rely=0.625)
 ventana_principal.protocol("WM_DELETE_WINDOW", ventana_principal.destroy)
 
 ventana_principal.mainloop() #Se mantiene la ventana creada abierta
--- Primer_avance_p2_micropython.py
+++ Primer_avance_p2_micropython.py
@@ -1,7 +1,53 @@
-from machine import Pin 
+from machine import Pin
+import _thread #Hace que se puedan hacer mas de una tarea a la vez
+import network #La libreria encargada de hacer que la raspy se pueda conectar
+import socket #La libreria que conecta la raspy con otro dispositivo
 from machine import ADC #ADC hace que la rasberry tenga pines que reciban datos analogicos
 from machine import PWM #PWM permite generar señales digitales periódicas (ondas cuadradas) para simular salidas analógicas
 import time #Se importa para hacer pausas entre instrucciones con el comando .sleep
+
+#================Configuracion a internet==================
+SSID     = "Andrew's phone"   # Nombre de la red WiFi
+PASSWORD = "soypobre"         # Contraseña de la red
+PORT     = 1717               # Puerto TCP del servidor
+
+def connect_wifi():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(SSID, PASSWORD)
+    
+    print("Conectando a WiFi...", end="")
+    while not wlan.isconnected():
+        print(".", end="")
+        time.sleep(0.5)
+    print("\nConectado:", wlan.ifconfig())
+    return wlan.ifconfig()[0]
+
+ip = connect_wifi()
+conn 
+def start_server():
+    global ip
+    s = socket.socket()
+    s.bind((ip, 1717))
+    s.listen(1)
+    print("Esperando conexión del cliente...")
+    conn, addr = s.accept()
+    print("Conectado desde:", addr)
+
+def recibir_mensaje():
+    global conn
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        msg = data.decode()
+        print("Mensaje recibido:", msg)
+        
+        # Acciones según el mensaje
+        conn.send(f"Echo: {msg}".encode())
+
+#====================================================
+
 
 #Se definen los pines que controlan los segmentos del 7 segmentos
 #Se tomo como ayuda para las conecciones el siguiente video: https://www.youtube.com/shorts/hsIBrn8cW7w
@@ -12,6 +58,9 @@ b  = Pin(16, Pin.OUT)
 a  = Pin(17, Pin.OUT)
 f  = Pin(18, Pin.OUT)
 g  = Pin(19, Pin.OUT)
+
+led_verde = Pin(12,Pin.OUT)
+led_rojo =  Pin(11,Pin.OUT)
 
 #Se agrega el pin del potenciometro (Con la configuracion ed ADC para que reciba señales analogicas
 # y no es necesario poner Pin.IN ya que por default se entiene que solo va a recibir, no dar señales) 
@@ -24,7 +73,7 @@ boton_servo = Pin(2, Pin.IN, Pin.PULL_DOWN)
 servo =  PWM(Pin(28)) #Por la naturaleza del PWM no es necesario el Pin.OUT
 servo.freq(50) #el 50 son l os hz en los que opera el servo motor
 v_0_grados = 500000 #Por el ciclo del servo, girara 500k nanosegundos hacia 0 grados
-v_180_grados = 2500000# Los 2.5 millones de nanosegundos representan un giro de 180
+v_180_grados = 2500000 # Los 2.5 millones de nanosegundos representan un giro de 180
 
 #Funcion que hace que el motor del servomotor gire de 0 a 180 grados
 def mover_servo():
@@ -72,26 +121,84 @@ def leer_numeros(num):
 def mostrar_numeros():
     for i in range(10):
         leer_numeros(i)
+#=====================================================
+#Las siguientes variables son la cantidad de los 3 productos que hay
+cantidad_de_x_producto = 0
+cantidad_de_y_producto = 3
+cantidad_de_z_producto = 1
+
+#Rangos de los valores del potenciometro para dividir el rango total del potenciometro
+#Se le asigna un producto a cada rango
+x = range(0,21880)
+y = range(21880, 43700)
+z = range(43700, 700000)
+
+#Esta funcion se activa cuando se pulsa el boton y recibe la lectura del potenciometro, determina en que rango se esta y si el producto asociado es diferente de 0.
+#si es diferente de 0, activa el servo motor y resta uno a la cantidad de producto
+def interaccion_producto_maquina(num):
+    global cantidad_de_x_producto, cantidad_de_y_producto, cantidad_de_z_producto #Para cambiar las variables de productos se llaman con global
+    
+    print(f"Hola, la lectura es {num}")
+    if num in x: #Este if y los otros elif determinan en que rango se esta, para saber con que producto interactuar
+        if cantidad_de_x_producto != 0: #Solo activa el servo si hay producto
+            mover_servo()
+            cantidad_de_x_producto -= 1
+            conn.send(f"Se ha comprado del producto X".encode())
+    elif num in y:
+        if cantidad_de_y_producto != 0:
+            mover_servo()
+            cantidad_de_y_producto -= 1
+            conn.send(f"Se ha comprado del producto Y".encode())
+    elif num in z:
+        if cantidad_de_z_producto != 0:
+            mover_servo()
+            cantidad_de_z_producto -= 1
+            conn.send("Se ha comprado del producto Z".encode())
+
+    
 #======================================================
 #Para el funcionamiento del potenciometro se uso como referencia el siguiente video: https://www.youtube.com/watch?v=IkbVy6IKhzU
 #Loop que toma constantemente el valor del potenciometro y lo asigna a un numero del 1 al 3 en el 7 segmentos dependiendo en el intervalo en el que este
-#Loop de prueba
+#Loop principal
+_thread.start_new_thread(recibir_mensaje, ())
 while True:
     lectura = poten.read_u16() #Variable que toma el valor del potenciometro
-    print(lectura)
-    time.sleep_ms(500)
-    if lectura < 43700 and lectura >= 21880:
+    #print(lectura)
+    time.sleep_ms(100)
+    if lectura in y:
         apagado()
-        leer_numeros(2)
-    elif lectura < 21880:
+        leer_numeros(cantidad_de_y_producto)
+        if cantidad_de_y_producto == 0:
+            led_verde.value(0)
+            led_rojo.value(1)
+        else:
+            led_verde.value(1)
+            led_rojo.value(0)
+    elif lectura in x:
         apagado()
-        leer_numeros(1)
+        leer_numeros(cantidad_de_x_producto)
+        if cantidad_de_x_producto == 0:
+            led_verde.value(0)
+            led_rojo.value(1)
+        else:
+            led_verde.value(1)
+            led_rojo.value(0)
     else:
         apagado()
-        leer_numeros(3)
+        leer_numeros(cantidad_de_z_producto)
+        if cantidad_de_z_producto == 0:
+            led_verde.value(0)
+            led_rojo.value(1)
+        else:
+            led_verde.value(1)
+            led_rojo.value(0)
         
+    time.sleep_ms(100)
     if boton_servo.value() == 1:
-        mover_servo()
+        interaccion_producto_maquina(lectura)
+        
+    time.sleep_ms(100)
+    
 #=============================================================================================================
 
 
